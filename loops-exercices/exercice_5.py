b2dedf1a5d8832077b49d5fddb52aaa9e84b5c31
Dylan Tettarasar <dtettarasar@gmail.com>
2021-12-12 07:13:40 +0100

    update on exercice 5

--- loops-exercices/exercice_5.py
+++ loops-exercices/exercice_5.py
@@ -1,38 +1,47 @@
 nombres = [1, 21, 5, 44, 4, 9, 5, 83, 29, 31, 25, 38]
-nombres_pairs = []
+nombres_pairs = [x for x in nombres if x % 2 == 0]
+"""
 for i in nombres:
     if i % 2 == 0:
         nombres_pairs.append(i)
+"""
 
 print(nombres_pairs)
 
 # ---------------------------------------------------- #
 
 nombres = range(-10, 10)
-nombres_positifs = []
+nombres_positifs = [x for x in nombres if x >= 0]
+"""
 for i in nombres:
     if i >= 0:
         nombres_positifs.append(i)
+"""
 
 print(nombres_positifs)
 
 # ---------------------------------------------------- #
 
 nombres = range(5)
-nombres_doubles = []
+nombres_doubles = [x*2 for x in nombres]
+"""
 for i in nombres:
     nombres_doubles.append(i * 2)
+"""
 
 print(nombres_doubles)
 
 # ---------------------------------------------------- #
 
 nombres = range(10)
-nombres_inverses = []
+nombres_inverses = [x if x % 2 == 0 else -x for x in nombres]
+
+"""
 for i in nombres:
     if i % 2 == 0:
         nombres_inverses.append(i)
     else:
         nombres_inverses.append(-i)
+"""
 
 print(nombres_inverses)
