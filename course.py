9e65467eb2d54eb697a128438d77f09f1fd3f7db
Dylan Tettarasar <dtettarasar@gmail.com>
2021-12-12 08:10:01 +0100

    course.py : display menu + handle invalid options

--- course.py
+++ course.py
@@ -1,5 +1,3 @@
-choix = ""
-
 def display_menu(separator):
     menu_str = """Choisissez parmi les 5 options suivantes : 
 1: Ajouter un élément à la liste
@@ -13,5 +11,16 @@ def display_menu(separator):
         print(menu_str)
     else:
         print(menu_str)
+    
+    choix = input("Votre choix : ")
+
+    if choix == "5":
+        print("A bientôt !")
+    else:
+        if choix.isdigit() and int(choix) in range(1,5):
+            print("ok")
+        else:
+            print("Veuillez saisir une option valide")
+        display_menu(True)
 
 display_menu(False)
