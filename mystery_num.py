188b560ee16e726b7b08bf7a09b26edc082ed757
Dylan Tettarasar CS50IDE <dtettarasar@gmail.com>
2021-12-16 17:03:34 +0000

    mystery_number.py: created comparison logic

--- mystery_num.py
+++ mystery_num.py
@@ -5,9 +5,17 @@ num_to_find = 75
 
 while essai != 0:
     print(f"Il te reste {essai} essai(s)")
-    player_num = input("Devine le nombre : ")
+    player_num_str = input("Devine le nombre : ")
 
-    if player_num.isdigit() == False:
-        print("Veuillez entrer un nombre valide.")
+    if player_num_str.isdigit() == False:
+        print("Tu dois entrer un nombre valide.")
     else:
+
+        player_num_int = int(player_num_str)
+
+        if num_to_find < player_num_int:
+            print(f"Le nombre mystère est plus petit que {player_num_int}.")
+        elif num_to_find > player_num_int:
+            print(f"Le nombre mystère est plus grand que {player_num_int}.")
+
         essai -= 1
