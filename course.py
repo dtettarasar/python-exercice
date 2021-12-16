74bb2bffce997403941fbe96039f4fc585e2b821
Dylan Tettarasar CS50IDE <dtettarasar@gmail.com>
2021-12-16 11:24:51 +0000

    course.py: update on the empty list

--- course.py
+++ course.py
@@ -32,29 +32,29 @@ def display_menu(separator):
         elif choix == "4":
             empty_list()
     else:
-        print("Veuillez saisir une option valide")
+        print("Veuillez saisir une option valide.")
     display_menu(True)
 
 def add_element():
     print("")
-    print("Vous avez choisi l'option 1 : Ajouter un élément à la liste")
+    print("Vous avez choisi l'option 1 : Ajouter un élément à la liste.")
     element_to_add = input("Entrez le nom d'un élément à ajouter à la liste : ")
     course_list.append(element_to_add)
     print(f"L'élément {element_to_add} à bien été ajouté à la liste.")
 
 def remove_element():
     print("")
-    print("Vous avez choisi l'option 2 : Retirer un élément de la liste")
+    print("Vous avez choisi l'option 2 : Retirer un élément de la liste.")
     element_to_remove = input("Entrez le nom d'un élément à retirer de la liste : ")
     if element_to_remove in course_list:
         course_list.remove(element_to_remove)
         print(f"L'élément {element_to_remove} à bien été retiré de la liste.")
     else:
-        print("Cet élément n'est pas dans la liste")
+        print("Cet élément n'est pas dans la liste.")
 
 def display_list():
     print("")
-    print("Vous avez choisi l'option 3 : Afficher la liste")
+    print("Vous avez choisi l'option 3 : Afficher la liste.")
     if not course_list:
         print("Votre liste est vide !")
     else:
@@ -63,10 +63,10 @@ def display_list():
             print(f"{i + 1}. {item}")
 
 def empty_list():
-    print("Vous avez choisi l'option 4 : Vider la liste")
+    print("Vous avez choisi l'option 4 : Vider la liste.")
     confirmation_message = """Etes vous sûr de vouloir vider la liste ?
-    1: Oui
-    2: Non
+    Tapez "oui" pour confirmer
+    Tapez autre chose pour annuler
     """
 
     if not course_list:
@@ -76,5 +76,10 @@ def empty_list():
     print(confirmation_message)
     choix = input("Votre choix : ")
 
+    if choix == "oui":
+        course_list.clear()
+        print("Votre liste a bien été vidée.")
+    else:
+        return False
 
 display_menu(False)
