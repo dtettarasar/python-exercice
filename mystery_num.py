9b49cd97ce8b8b0a54ac257c7de25243448ab42b
Dylan Tettarasar CS50IDE <dtettarasar@gmail.com>
2021-12-19 11:48:55 +0000

    mystery num : update message for wins

--- mystery_num.py
+++ mystery_num.py
@@ -1,6 +1,6 @@
 print("--- le jeu du nombre mystère :) ---")
 
-essai = 5
+essai = const_essai = 5
 num_to_find = 75
 
 while essai != 0:
@@ -18,7 +18,9 @@ while essai != 0:
         elif num_to_find > player_num_int:
             print(f"Le nombre mystère est plus grand que {player_num_int}.")
         else:
-            print("Bravo!")
+            print(f"Bravo ! Le nombre mystère était bien {player_num_int} !")
+            print(f"Tu as trouvé le nombre en {const_essai + 1 - essai} essai(s)")
+            print("Fin du jeu.")
             break
 
         essai -= 1
