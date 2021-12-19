598f105b5711d3260b3a0ab9fc92c3705a337354
Dylan Tettarasar CS50IDE <dtettarasar@gmail.com>
2021-12-19 11:29:34 +0000

    mystery num: add condition if find right number

--- mystery_num.py
+++ mystery_num.py
@@ -17,5 +17,8 @@ while essai != 0:
             print(f"Le nombre mystère est plus petit que {player_num_int}.")
         elif num_to_find > player_num_int:
             print(f"Le nombre mystère est plus grand que {player_num_int}.")
+        else:
+            print("Bravo!")
+            break
 
         essai -= 1
