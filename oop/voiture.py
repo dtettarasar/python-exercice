1e0abb1f48fc2bb634e4d6c61400e3fcf998145f
Dylan Tettarasar <dtettarasar@gmail.com>
2022-02-01 21:57:12 +0100

    voiture.py : created roule method

--- oop/voiture.py
+++ oop/voiture.py
@@ -5,8 +5,15 @@ class Voiture():
     def afficher_reservoir(self):
         print(f"La voiture contient {self.essence}L d'essence")
 
+    def roule(self, km):
+        essence_utilise = (km * 5)/100
+        self.essence -= essence_utilise
+        self.afficher_reservoir()
+
 if __name__ == "__main__":
     print("module voiture.py")
     test_voiture = Voiture()
     print(test_voiture.essence)
     test_voiture.afficher_reservoir()
+    for i in range(4):
+        test_voiture.roule(25)
