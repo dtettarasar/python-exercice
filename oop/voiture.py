a70ada604e8565dfdd7a8aca49872693981688c7
Dylan Tettarasar <dtettarasar@gmail.com>
2022-02-01 22:06:36 +0100

    voiture.py: update on roule() method

--- oop/voiture.py
+++ oop/voiture.py
@@ -7,7 +7,14 @@ class Voiture():
 
     def roule(self, km):
         essence_utilise = (km * 5)/100
-        self.essence -= essence_utilise
+        if self.essence == 0:
+            print("Vous n'avez plus d'essence, faites le plein !")
+        elif self.essence < essence_utilise:
+            print(f"Vous n'avez pas assez d'essence pour parcourir {km} km, faites le plein !")
+        else:
+            self.essence -= essence_utilise
+            if self.essence < 10:
+                print("Vous n'avez bientôt plus d'essence !")
         self.afficher_reservoir()
 
 if __name__ == "__main__":
@@ -15,5 +22,5 @@ if __name__ == "__main__":
     test_voiture = Voiture()
     print(test_voiture.essence)
     test_voiture.afficher_reservoir()
-    for i in range(4):
-        test_voiture.roule(25)
+    for i in range(25):
+        test_voiture.roule(85)
