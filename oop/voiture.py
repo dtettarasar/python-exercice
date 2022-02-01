cb7e6e7a32e388330b0d6e17177db22aa6476bdf
Dylan Tettarasar <dtettarasar@gmail.com>
2022-02-01 21:52:18 +0100

    voiture.py : created afficher essence method

--- oop/voiture.py
+++ oop/voiture.py
@@ -2,7 +2,11 @@ class Voiture():
 
     essence = 100
 
+    def afficher_reservoir(self):
+        print(f"La voiture contient {self.essence}L d'essence")
+
 if __name__ == "__main__":
     print("module voiture.py")
     test_voiture = Voiture()
     print(test_voiture.essence)
+    test_voiture.afficher_reservoir()
