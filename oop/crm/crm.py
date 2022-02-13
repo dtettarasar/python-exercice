80cf14bb683d71ea44faf45bfc5fedc961ed5ae2
Dylan Tettarasar <dtettarasar@gmail.com>
2022-02-13 16:59:58 +0100

    crm.py: created check phone number method in user class

--- oop/crm/crm.py
+++ oop/crm/crm.py
@@ -1,3 +1,4 @@
+import re
 class User:
     def __init__(self, first_name: str, last_name: str, phone_number: str="", address: str=""):
         self.first_name = first_name
@@ -15,17 +16,26 @@ class User:
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def _check_phone_number(self):
+        phone_number = re.sub(r"[+()\s]*", "", self.phone_number)
+        # print(phone_digits)
+
+        if len(phone_number) < 10 or not phone_number.isdigit():
+            raise ValueError(f"Following phone number not valid: {self.phone_number}")
+
 if __name__ == "__main__":
     from faker import Faker
     fake = Faker(locale="fr_FR")
     print("User class module")
+    print("-" * 15)
 
-    for _ in range(10):
+    for _ in range(4):
         user = User(
             first_name = fake.first_name(),
             last_name = fake.last_name(),
             phone_number = fake.phone_number(),
             address = fake.address())
         print(user)
-        print(repr(user))
+        # print(repr(user))
+        user._check_phone_number()
         print("-" * 15)
